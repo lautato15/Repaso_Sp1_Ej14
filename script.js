2a316ad27b8999715353511a8f68e4a898fccbe1
lautato15 <lautato15@gmail.com>
2024-04-05 15:31:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ function convert(num, str) {
 // Crear una función de JavaScript llamada convert (Convertir) que reciba como parámetros un número y un string. El primer parámetro corresponde a una distancia (número) y el segundo parámetro corresponde a una unidad (string) que puede ser kilómetros ("km") o millas ("mi"). La función debe retornar un string con la cantidad ingresada convertida a la otra unidad (millas o kilómetros).
 // Nota: El número resultante no puede tener más de 2 decimales, por lo que es necesario hacer el redondeo correspondiente.
 // // Ejemplos:
-// Input
-// Output
+// Input --------------> Output
 // convert(0, "mi") ---> "0 km"
 // convert(0, "km") ---> "0 mi"
 // convert(100, "mi") ---> "160.93 km"
